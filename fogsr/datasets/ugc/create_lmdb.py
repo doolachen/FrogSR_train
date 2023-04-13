79367319fc048c628f0b8c52f7a3b87dace2ee5a
yindaheng98 <yindaheng98@gmail.com>
2023-04-13 12:13:12 +0000

    大号数据集也就多个6万张图

--- fogsr/datasets/ugc/create_lmdb.py
+++ fogsr/datasets/ugc/create_lmdb.py
@@ -2,7 +2,14 @@ from os import listdir, makedirs
 from os import path as osp
 
 from fogsr.datasets.lmdb_util import make_lmdb_from_imgs
-from fogsr.datasets.ugc.config import train_list
+from fogsr.datasets.ugc.config import (
+    large_train_list as train_list,
+    large_gt_folder_path as gt_folder_path,
+    large_gt_lmdb_path as gt_lmdb_path,
+    large_lq_folder_path as lq_folder_path,
+    large_lq_lmdb_path as lq_lmdb_path,
+)
+
 
 def create_lmdb_for_ugc():
     """Create lmdb files for YTB-UGC dataset.
@@ -11,19 +18,19 @@ def create_lmdb_for_ugc():
         Remember to modify opt configurations according to your settings.
     """
     # GT
-    folder_path = osp.expanduser('~/dataset/ugc-dataset-image/vp9_compressed_videos')
-    lmdb_path = osp.expanduser('~/dataset/ugc-dataset-lmdb/vp9_compressed_videos/train_GT.lmdb')
+    folder_path = gt_folder_path
+    lmdb_path = gt_lmdb_path
     img_path_list, keys = prepare_keys_ugc(folder_path, train_list,
-                                                'gt')
+                                           'gt')
     makedirs(osp.dirname(lmdb_path), exist_ok=True)
     make_lmdb_from_imgs(
         folder_path, lmdb_path, img_path_list, keys, multiprocessing_read=True)
 
     # LQ
-    folder_path = osp.expanduser('~/dataset/ugc-dataset-image/vp9_compressed_videos_x4lossless')
-    lmdb_path = osp.expanduser('~/dataset/ugc-dataset-lmdb/vp9_compressed_videos/train_LR.lmdb')
+    folder_path = lq_folder_path
+    lmdb_path = lq_lmdb_path
     img_path_list, keys = prepare_keys_ugc(folder_path, train_list,
-                                                'lq')
+                                           'lq')
     makedirs(osp.dirname(lmdb_path), exist_ok=True)
     make_lmdb_from_imgs(
         folder_path, lmdb_path, img_path_list, keys, multiprocessing_read=True)
