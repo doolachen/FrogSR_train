9183b6f482e1af7767b4d959678bafde1c407967
yindaheng98 <yindaheng98@gmail.com>
2023-04-16 13:32:21 +0000

    gt有问题

---
+++ config.py
@@ -0,0 +1,49 @@
+
+from fogsr.data import build_dataloader, build_dataset
+
+
+def REDS_opt(mode='folder'):
+    """Test reds dataset.
+    Args:
+        mode: There are two modes: 'lmdb', 'folder'.
+    """
+    opt = {}
+    opt['dist'] = False
+    opt['phase'] = 'train'
+
+    opt['name'] = 'REDS'
+    opt['type'] = 'REDSDataset'
+    if mode == 'folder':
+        opt['dataroot_gt'] = '/home/cbj/dataset/REDS/train/train_sharp'
+        opt['dataroot_lq'] = '/home/cbj/dataset/REDS/train/train_sharp_bicubic'
+        opt['dataroot_flow'] = None
+        opt['meta_info_file'] = 'fogsr/data/meta_info/meta_info_REDS_GT.txt'
+        opt['io_backend'] = dict(type='disk')
+    elif mode == 'lmdb':
+        opt['dataroot_gt'] = '/home/cbj/dataset/REDS/train/train_sharp_with_val.lmdb'
+        opt['dataroot_lq'] = '/home/cbj/dataset/REDS/train/train_sharp_bicubic_with_val.lmdb'
+        opt['dataroot_flow'] = None
+        opt['meta_info_file'] = 'fogsr/data/meta_info/meta_info_REDS_GT.txt'
+        opt['io_backend'] = dict(type='lmdb')
+
+    opt['val_partition'] = 'REDS4'
+    opt['num_frame'] = 7
+    opt['gt_size'] = 256
+    opt['interval_list'] = [1]
+    opt['random_reverse'] = True
+    opt['use_hflip'] = True
+    opt['use_rot'] = True
+
+    opt['num_worker_per_gpu'] = 1
+    opt['batch_size_per_gpu'] = 2
+    opt['scale'] = 4
+
+    opt['dataset_enlarge_ratio'] = 1
+    
+    return opt
+
+def REDS_dataloader(mode='folder'):
+    opt = REDS_opt(mode=mode)
+    dataset = build_dataset(opt)
+    data_loader = build_dataloader(dataset, opt, num_gpu=0, dist=opt['dist'], sampler=None)
+    return data_loader
--- vrt_test.py
+++ vrt_test.py
@@ -9,29 +9,36 @@ from fogsr.datasets import restore_images
 from fogsr.trainer.loss import structural_similarity
 from fogsr.trainer.vrt_util import test_vrt
 
-path = "vrt_test"
+path = os.path.join("lightning_logs", "vrt_test")
 os.makedirs(path, exist_ok=True)
 
+def model_large():
+    from fogsr.models.vrt import VRT_Dv3
+    from fogsr.models.vrt import VRTDv3_videosr_bi_Vimeo_7frames
+    test_args = VRTDv3_videosr_bi_Vimeo_7frames['wrapper']['test_args']
+    return VRT_Dv3(**VRTDv3_videosr_bi_Vimeo_7frames['model']), test_args
+
+def dataloader_ugc():
+    pass
+
+def dataloader_reds():
+    from config import REDS_dataloader
+    return REDS_dataloader()
 
 def main():
-    config = parse_config()
-    model = parse_model(config)
-    model, test_loader = model.model, model.val_dataloader()
+    model, test_args = model_large()
+    test_loader = dataloader_reds()
+
     model.eval()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Model to {device}")
     model = model.to(device)
 
     for idx, batch in enumerate(test_loader):
-        lq = batch['LRs'].to(device)
-        gt = batch['HRs']
-        # lq, gt = crop(lq, gt)
-        f_lr = open(os.path.join(path, "vid4_lr_%d.yuv" % idx), 'wb')
-        f_hr = open(os.path.join(path, "vid4_hr_%d.yuv" % idx), 'wb')
-        f_gt = open(os.path.join(path, "vid4_gt_%d.yuv" % idx), 'wb')
-        # inference
+        lq = batch['lq'].to(device)
+        gt = batch['gt']
         with torch.inference_mode():
-            output = test_vrt(lq, model, **config['wrapper']['test_args'])
+            output = test_vrt(lq, model, **test_args)
             print(output.shape)
             input_images = restore_images(lq)
             output_images = restore_images(output)
@@ -46,16 +53,7 @@ def main():
                 cv2.imwrite(os.path.join(path, "vid4_%d_%02d_LR.png" % (idx, j)), lr)
                 cv2.imwrite(os.path.join(path, "vid4_%d_%02d_HR.png" % (idx, j)), output)
                 cv2.imwrite(os.path.join(path, "vid4_%d_%02d_GT.png" % (idx, j)), target)
-                lr_yuv = cv2.cvtColor(lr, cv2.COLOR_BGR2YUV_I420)
-                hr_yuv = cv2.cvtColor(output, cv2.COLOR_BGR2YUV_I420)
-                gt_yuv = cv2.cvtColor(target, cv2.COLOR_BGR2YUV_I420)
-                f_lr.write(lr_yuv.astype(np.uint8).tobytes())
-                f_hr.write(hr_yuv.astype(np.uint8).tobytes())
-                f_gt.write(gt_yuv.astype(np.uint8).tobytes())
             print('PSNR / SSIM', psnr_all / n, '/', ssim_all / n)
-            f_lr.close()
-            f_hr.close()
-            f_gt.close()
 
 
 if __name__ == '__main__':
